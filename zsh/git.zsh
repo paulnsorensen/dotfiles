4286a10d5b2ef44154eea52bc0809ead6f3c37f9
Paul Sorensen <paulnsorensen@gmail.com>
2017-08-22 17:58:47 -0700

    Add more git aliases

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,9 +1,14 @@
+# generally this is a subset from here: https://github.com/robbyrussell/oh-my-zsh/wiki/Plugin:git
+
 alias ga='git add'
 alias gb='git branch'
 alias gco='git checkout'
-alias gcb='git checkout --branch'
+alias gcb='git checkout -b'
 alias gc='git commit -v'
+alias gcm='git commit -m'
 alias gd='git diff'
 alias gdn='git diff --name-only'
+alias gf='git fetch'
+alias gl='git pull'
 alias gp='git push'
 alias gst='git status'
